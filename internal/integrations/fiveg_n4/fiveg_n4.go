8ee419f8a54f0469679862422476541e317c6368
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-03-30 17:47:07 -0400

    chore: bump goops to v0.0.5 (#3)
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- internal/integrations/fiveg_n4/fiveg_n4.go
+++ internal/integrations/fiveg_n4/fiveg_n4.go
@@ -3,16 +3,16 @@ package fiveg_n4
 import (
 	"fmt"
 
-	"github.com/gruyaume/goops/commands"
+	"github.com/gruyaume/goops"
 )
 
-func PublishUPFN4Information(hookCommand *commands.HookCommand, relationID string, hostname string, port int) error {
+func PublishUPFN4Information(hookContext *goops.HookContext, relationID string, hostname string, port int) error {
 	portStr := fmt.Sprintf("%d", port)
 	relationData := map[string]string{
 		"upf_hostname": hostname,
 		"upf_port":     portStr,
 	}
-	err := commands.RelationSet(hookCommand, relationID, true, relationData)
+	err := hookContext.Commands.RelationSet(relationID, true, relationData)
 	if err != nil {
 		return fmt.Errorf("could not set relation data: %w", err)
 	}
